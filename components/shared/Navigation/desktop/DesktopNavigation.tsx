605c94403f031552b2459773858a7059d9ef8146
Johnathan Guzman <johnathanthedev@gmail.com>
2024-03-19 22:33:02 -0500

    (Enhancement) Add no selection text for map

--- components/shared/Navigation/desktop/DesktopNavigation.tsx
+++ components/shared/Navigation/desktop/DesktopNavigation.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import FacilityIcon from "@/components/shared/icons/FacilityIcon";
-import HomeIcon from "@/components/shared/icons/HomeIcon";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import styles from "./index.module.css";
@@ -15,17 +13,9 @@ export default function DesktopNavigation() {
   return (
     <div className={styles.container}>
       <div className={styles.logoWrapper}>
-        <Image src={logo} alt="Supply Chain Management Globe logo" />
+        <Image onClick={() => router.push("/dashboard")} src={logo} alt="Supply Chain Management Globe logo" />
       </div>
       <div className={styles.navigationLinksContainer}>
-        <div onClick={() => router.push("/dashboard")} className={`${currentPath === "/dashboard" && styles.active} ${styles.navigationItem}`}>
-          <HomeIcon color={`${currentPath === "/dashboard" ? "var(--brand-color)" : "var(--text-color-100)"}`} />
-          <span>Home</span>
-        </div>
-        <div onClick={() => router.push("/facilities")} className={`${currentPath === "/facilities" && styles.active} ${styles.navigationItem}`}>
-          <FacilityIcon color={`${currentPath === "/facilities" ? "var(--brand-color)" : "var(--text-color-100)"}`} />
-          <span>Facilities</span>
-        </div>
       </div>
       <div className={styles.pfpWrapper}>
         <Image src={pfp} alt="Temporary profile picture icon" />
